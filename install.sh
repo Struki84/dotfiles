2ccb148de42a6700d786100db0005d8f3f952bc4
Šimun Strukan <simun.strukan@gmail.com>
2024-02-07 21:22:19 +0100

    Setting up first version

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 # source tmux to custom conf
 # source zsh to custom conf
 # add custom nvim conf to astro nvim conf
+# vim.cmd([[luafile ~/.dotfiles/nvim/my_init.lua]])
+# need to install nerd fonts
+# brew tap homebrew/cask-fonts && brew install --cask font-<FONT NAME>-nerd-font || FOR OSX
+
