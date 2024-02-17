592c9dc35cbe70cd91c89441d89c0fac8811b111
Simun Strukan <simun@korriban.local>
2024-02-17 23:35:55 +0100

    added go setup

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -2,3 +2,7 @@
 #############################################
 export GO111MODULE=on
 export GOPRIVATE=gitlab.sintezis.co
+
+export PATH=$PATH:/usr/local/go
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin:$PATH
