7f461e9701b13f0924fad4eb1ed61bc6cae48d23
Simun <simun.strukan@gmail.com>
2025-08-17 00:03:54 +0200

    debugging install scripts

--- arch/install/apps.sh
+++ arch/install/apps.sh
@@ -5,7 +5,7 @@ yay -S --noconfirm --needed \
   virtualbox virtualbox-host-modules-arch localsend-bin libreoffice-fresh vlc \
   plex-desktop solaar resilio-sync
 
-# Setup resilion sync
+# Setup resilio sync
 
 cp ~/.local/share/dotfiles/arch/applications/*.desktop ~/.local/share/applications/
 cp ~/.local/share/dotfiles/arch/applications/hidden/*.desktop ~/.local/share/applications/
