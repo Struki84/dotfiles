0ceb9f1a98cf91039f61c3d56ab6f6daaf6ebe92
Simun <simun.strukan@gmail.com>
2025-12-04 21:00:22 +0100

    Adding python env helpers

--- arch/shell/zsh/aliases.zsh
+++ arch/shell/zsh/aliases.zsh
@@ -1,7 +1,3 @@
-### My Python aliases
-alias python="/usr/bin/python3"
-alias pip="/usr/bin/pip3"
-
 ### Navigation shortcuts
 alias ls="eza -l --icons --group-directories-first"
 alias lst="ls -TL 2"
@@ -17,3 +13,28 @@ alias clipt="~/Engineering/Libraries/clipt/"
 
 export SUDO_EDITOR="nvim"
 alias "sudoedit"='function _sudoedit(){sudo -e "$1";};_sudoedit'
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  python() {
+    if [[ -n "${VIRTUAL_ENV}" ]]; then
+      "${VIRTUAL_ENV}/bin/python" "$@"
+    else
+      command python3 "$@"
+    fi
+  }
+fi
+
+# Auto-activate pyenv version + venv when entering a project
+function cd() {
+  builtin cd "$@"
+
+  if [ -f .python-version ] && command -v pyenv >/dev/null; then
+    pyenv local $(cat .python-version)
+  fi
+
+  if [ -d ".venv" ]; then
+    source .venv/bin/activate 2>/dev/null || true
+  elif [ -n "$VIRTUAL_ENV" ] && [ "$PWD" != "$(dirname $VIRTUAL_ENV)"* ]; then
+    deactivate 2>/dev/null || true
+  fi
+}
